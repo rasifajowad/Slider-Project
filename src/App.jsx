e5eae6090ab07325756c54abcdaaf12a2f0383c4
rasifajowad <120589758+rasifajowad@users.noreply.github.com>
2023-11-19 20:11:19 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { list } from "./data";
 import Carousel from "./Carousel";
+import AlternativeCarousel from "./AlternativeCarousel";
 
 const App = () => {
   return (
     <main>
+      <h1 style={{ textAlign: "center", marginTop: "2rem" }}>
+        Carousel (Own Implementation)
+      </h1>
       <Carousel list={list} />
+      <h1 style={{ textAlign: "center" }}>Carousel (React-Slider)</h1>
+      <AlternativeCarousel list={list} />
     </main>
   );
 };
